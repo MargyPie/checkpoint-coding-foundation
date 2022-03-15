4537b0f6686bc684ff32d6ad40240d0a4083d696
Martina Recke <tinyty@web.de>
2022-03-15 21:02:57 +0100

    feat: adds kata instructions to all 3 files

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,45 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  //The first parameter apiData will be an array that includes objects
+  //The objects can have different keys
+  const apiData = [
+    {
+      id: 1,
+      description: "",
+      price: 0,
+      title: "",
+    },
+    {
+      price: 0,
+      tags: [],
+    },
+    {
+      id: 3,
+      price: 0,
+      title: "",
+    },
+  ];
+
+  //The second parameter mandatoryKeys is a array that includes all keys
+  //that an object (from the apiData array) should have to pass the filter test
+  const mandatoryKeys = [filterApiData(apiData, ["id"])];
+
+  //Return an array with only the objects that include all keys from the mandatoryKeys array
+  
 }
+
+/*
+Filter Api Data Examples
+const data = [{ id: 1, price: 100 }, { price: 50 }];
+
+const filteredData = filterApiData(data, ["id"]);
+
+console.log(filteredData); // [{id: 1, price: 0}]
+const data = [
+  { id: 1, price: 100 },
+  { id: 2, title: "" },
+  { id: 3, price: 50 },
+];
+
+const filteredData = filterApiData(data, ["id", "price"]);
+
+console.log(filteredData); // [{ id: 1, price: 100 }, { id:3, price: 50}]*/
--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,16 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  //The zipStrings function should return one string where the characters of both parameters
+  //are merged using the zip procedure (Reißverschlussverfahren)
 }
+
+/*
+Zip Strings Example
+console.log(zipStrings("abc", "123")); // "a1b2c3"
+
+console.log(zipStrings("abc", "1")); // "a1bc"
+
+console.log(zipStrings("a", "123")); // "a123"
+
+console.log(zipStrings("", "123")); // "123"
+
+console.log(zipStrings("abc", "")); // "abc"*/
