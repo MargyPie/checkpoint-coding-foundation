13630271744d7405a7be5ae22960553c4ac57615
Martina Recke <tinyty@web.de>
2022-03-16 20:26:53 +0100

    feat: finishes zip-string kata and passes all tests

--- zip-strings.js
+++ zip-strings.js
@@ -1,16 +1,33 @@
 function zipStrings(strA, strB) {
   //The zipStrings function should return one string where the characters of both parameters
   //are merged using the zip procedure (Reißverschlussverfahren)
+
+  //Convert Str to Arr
+  let arrA = strA.split("");
+  let arrB = strB.split("");
+
+  let zipArr = [];
+
+  if (arrA.length >= arrB.length) {
+    for (let i = 0; i < arrA.length; i++) {
+      if (i < arrB.length && i < arrA.length) {
+        zipArr.push(arrA[i] + arrB[i]);
+      } else if (i >= arrB.length) {
+        zipArr.push(arrA[i]);
+      } else {
+        zipArr.push(arrB[i]);
+      }
+    }
+  }
+  // If strB is longer than strA return strB
+  else {
+    for (let i = 0; i < arrB.length; i++) {
+      zipArr.push(arrB[i]);
+    }
+  }
+
+  //Convert back to string
+  let zip = zipArr.join("");
+
+  return zip;
 }
-
-/*
-Zip Strings Example
-console.log(zipStrings("abc", "123")); // "a1b2c3"
-
-console.log(zipStrings("abc", "1")); // "a1bc"
-
-console.log(zipStrings("a", "123")); // "a123"
-
-console.log(zipStrings("", "123")); // "123"
-
-console.log(zipStrings("abc", "")); // "abc"*/
