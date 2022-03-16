130445ef61944000643fb432f374c905a2ef0f17
Martina Recke <tinyty@web.de>
2022-03-16 20:58:19 +0100

    feat: finishes sum-app kata and passes all tests

--- sum-app.js
+++ sum-app.js
@@ -1,7 +1,7 @@
 class SumApp {
   //The sumApp class should have a property numbers which is an array
   constructor(numbers) {
-    this.numbers = numbers;
+    this.numbers = [];
   }
 
   //The sumApp class should have a method addNumber(n) which will add the given number to the numbers array
@@ -9,11 +9,11 @@ class SumApp {
     this.numbers.push(n);
   }
 
-  //The sumApp class should have a method getSum() which return the sum of all numbers added to the numbers array
+  //The sumApp class should have a method getSum() which returns the sum of all numbers added to the numbers array
   //The getSum() method should return 0 if no numbers have been added to the numbers array
   getSum() {
     let sum = 0;
-    for (num of this.numbers) {
+    for (const num of this.numbers) {
       sum += num;
     }
     return sum;
@@ -42,4 +42,4 @@ sumApp.reset();
 
 console.log(sumApp.numbers.length); // 0
 console.log(sumApp.getSum()); // 0
-*/ 
+*/
