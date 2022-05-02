6e42167e0af603726872e48cc57bf5d580e1961e
Martina Recke <tinyty@web.de>
2022-05-02 21:30:53 +0200

    feat: implements the filter-api-data function

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,45 +1,23 @@
 function filterApiData(apiData, mandatoryKeys) {
-  //The first parameter apiData will be an array that includes objects
-  //The objects can have different keys
-  const apiData = [
-    {
-      id: 1,
-      description: "",
-      price: 0,
-      title: "",
-    },
-    {
-      price: 0,
-      tags: [],
-    },
-    {
-      id: 3,
-      price: 0,
-      title: "",
-    },
-  ];
+  let result = [];
 
-  //The second parameter mandatoryKeys is a array that includes all keys
-  //that an object (from the apiData array) should have to pass the filter test
-  const mandatoryKeys = [filterApiData(apiData, ["id"])];
+  for (let i = 0; i < apiData.length; i++) {
+    const toBeCheckedData = apiData[i];
+    const apiDataKeys = Object.keys(toBeCheckedData);
+    let containsAllKeys = false;
 
-  //Return an array with only the objects that include all keys from the mandatoryKeys array
-  
+    for (let j = 0; j < mandatoryKeys.length; j++) {
+      if (apiDataKeys.includes(mandatoryKeys[j])) {
+        containsAllKeys = true;
+      } else {
+        containsAllKeys = false;
+        break;
+      }
+    }
+
+    if (containsAllKeys) {
+      result.push(toBeCheckedData);
+    }
+  }
+  return result;
 }
-
-/*
-Filter Api Data Examples
-const data = [{ id: 1, price: 100 }, { price: 50 }];
-
-const filteredData = filterApiData(data, ["id"]);
-
-console.log(filteredData); // [{id: 1, price: 0}]
-const data = [
-  { id: 1, price: 100 },
-  { id: 2, title: "" },
-  { id: 3, price: 50 },
-];
-
-const filteredData = filterApiData(data, ["id", "price"]);
-
-console.log(filteredData); // [{ id: 1, price: 100 }, { id:3, price: 50}]*/
